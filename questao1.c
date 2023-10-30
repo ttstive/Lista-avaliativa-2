4f841d1e567e0e190308703b0c2af456b2a71ecb
Estevão Lins Maia <114420545+ttstive@users.noreply.github.com>
2023-10-30 10:21:19 -0300

    Update questao1.c

--- questao1.c
+++ questao1.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-
-//uma função para somar vetores, ou seja, quero que cada elemento do meu vetor seja somado de 2 em 2
-// até que reste somente 1 valor e nao tenha mais como somar;
+// Uma função para somar vetores, ou seja, quero que cada elemento do meu vetor seja somado de 2 em 2
+// até que reste somente 1 valor e não tenha mais como somar;
 
 int somaVetores(int valores[], int tamanho) {
-    while (tamanho >1) {
+    while (tamanho > 1) {
         for (int i = 0; i < tamanho - 1; i++) {
             valores[i] = valores[i] + valores[i + 1];
-        } // o while so vai funcionar enquanto meu vetor tiver mais que um valor, somando de 2 em 2 até que reste somente 1
+        } // o while só vai funcionar enquanto meu vetor tiver mais que um valor, somando de 2 em 2 até que reste somente 1
         tamanho--;  // Atualiza o tamanho da lista
         for (int i = 0; i < tamanho; i++) {
-            printf("%d ", valores[i]); // tem somente um backspace apos %d, para que fiquem com espaçamento conforme o que foi pedido
+            printf("%d", valores[i]);
+            if (i < tamanho - 1) {
+                printf(" "); // Adicione um espaço entre os valores, exceto para o último valor
+            }
         }
         printf("\n");
     }
@@ -24,13 +26,9 @@ int main() {
     printf("\n");
 
     for (int i = 0; i < n_elementos; i++) {
-        scanf("%d ", &conjunto_de_valores[i]);
+        scanf("%d", &conjunto_de_valores[i]);
     }
 
-    for (int i = 0; i < n_elementos; i++) {
-        printf("%d", conjunto_de_valores[i]);
-    }
-    printf("\n");
 
     somaVetores(conjunto_de_valores, n_elementos);
 
